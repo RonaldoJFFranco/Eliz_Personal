43a13aab92272f7a0696e8e5f860716d95eff843
Ronaldo Franco <ronaldojffranco72@gmail.com>
2026-08-28 16:59:37 -0300

    teste analytics

--- artifacts/eliz-personal/src/main.tsx
+++ artifacts/eliz-personal/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import { ErrorBoundary } from '@/components/error-boundary';
-
+import { Analytics } from "@vercel/analytics/react"
 import './index.css';
 
 createRoot(document.getElementById('root')!, {
@@ -13,5 +13,6 @@ createRoot(document.getElementById('root')!, {
 }).render(
   <ErrorBoundary>
     <App />
+    <Analytics/>
   </ErrorBoundary>,
 );
